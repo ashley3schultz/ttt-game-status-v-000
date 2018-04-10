521a7fdab3f76b35fa962ad84bddec5769270d7a
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-10 17:22:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,15 +6,15 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.select do |i|
+  WIN_COMBINATIONS.detect do |i|
     i1 = i[0]
     i2 = i[1]
     i3 = i[2]
     bi = [board[i1],board[i2],board[i3]]
     if bi.all? {|ck| ck == "X"} || bi.all? {|ck| ck == "O"}
-      TRUE
+      bi.select {|winner| winner
     else bi.all? {|ck| ck != "X"} || bi.all? {|ck| ck != "O"}
-      FALSE
+      nil
     end
   end
 end
