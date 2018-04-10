022db9fe2eb5493e942965877fbc5314d52c9136
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-10 17:00:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,69 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |i|
+    i1 = i[0]
+    i2 = i[1]
+    i3 = i[2]
+    bi = [board[i1],board[i2],board[i3]]
+    if bi.all? {|ck| ck == "X"} || bi.all? {|ck| ck == "O"}
+    TRUE
+  else
+    FALSE
+    end
+  end
+end
+
+
+
+def full?(board)
+  board.none? do |i|
+    if i == " "
+      TRUE
+    end
+  end
+end
+
+
+
+def draw?(board)
+  if (won?(board) == FALSE && full?(board) == TRUE)
+    TRUE
+  else (won?(board) == FALSE && full?(board) == FALSE)
+    FALSE
+  end
+end
+
+
+
+def over?(board)
+  if won?(board) != FALSE
+    TRUE
+  else full?(board) == TRUE
+    TRUE
+  end
+end
+
+
+def winner(board)
+  if draw?(board) == TRUE
+    nil
+  else won?(board) != FALSE
+    WIN_COMBINATIONS.find do |i|
+      i1 = i[0]
+      i2 = i[1]
+      i3 = i[2]
+      bi = [board[i1],board[i2],board[i3]]
+      if bi.all? {|ck| ck == "X"}
+        winner = "X"
+      else bi.all? {|ck| ck == "O"}
+        winner = "O"
+      end
+    end
+    winner
+  end
+end
+  
