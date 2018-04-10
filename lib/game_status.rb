60cdcff61c9b9ca2da45fd59277259660bbc557c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-10 18:24:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,8 +11,10 @@ def won?(board)
     wi2 = wc[1]
     wi3 = wc[2]
     bi = [board[wi1],board[wi2],board[wi3]]
-    bi.all? {|x| x == "X"} || bi.all? {|o| o == "O"}
+    if (bi.all? {|x| x == "X"} || bi.all? {|o| o == "O"})
       wc
+      end
+  TRUE
   else
     FALSE
     end
